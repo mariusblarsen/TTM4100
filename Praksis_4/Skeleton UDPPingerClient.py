6a31717ba16e6af93cba3be31676e4682eca2929
Marius Bæver Larsen <mariubla@stud.ntnu.no>
2020-02-28 13:40:14 +0100

    Added client to server ping and receive

--- Praksis_4/Skeleton UDPPingerClient.py
+++ Praksis_4/Skeleton UDPPingerClient.py
@@ -3,53 +3,58 @@ import sys
 from socket import * # Don't need to specify socket.method()
 
 # Get the server hostname and port as command line arguments                    
-host =  sys.argv[0]
-port = sys.argv[1]
-timeout = 1 # in seconds
- 
-# Create UDP client socket
-clientSocket = socket(AT_INET, SOCK_DGRAM)
-clientSocket.bind((host, port))
+def main():
+    host =  sys.argv[0]
+    print("host: ", host)
+    port = sys.argv[1]
+    print("port: ", port)
+    timeout = 1 # in seconds
+     
+    # Create UDP client socket
+    clientSocket = socket(AT_INET, SOCK_DGRAM) #Internet and UDP
+    clientSocket.bind((host, port))
 
-# Note the second parameter is NOT SOCK_STREAM
-# but the corresponding to UDP
-
-# Set socket timeout as 1 second
-clientSocket.settimeout(timeout)
+    # Set socket timeout as 1 second
+    clientSocket.settimeout(timeout)
 
 
-# Sequence number of the ping message
-ptime = 0  
+    # Sequence number of the ping message
+    ptime = 0  
 
-# Ping for 10 times
-while ptime < 10: 
-    ptime += 1
-    # Format the message to be sent as in the Lab description	
-    data = # FILL IN START		# FILL IN END
-    
-    try:
-    	# FILL IN START
-    	
-	# Record the "sent time"
-
-	# Send the UDP packet with the ping message
-
-	# Receive the server response
-  
-	# Record the "received time"
-
-	# Display the server response as an output
-    
-	# Round trip time is the difference between sent and received time
-
+    # Ping for 10 times
+    while ptime < 10: 
+        ptime += 1
+        # Format the message to be sent as in the Lab description	
+        data = "Ping " + ptime # message should be: Ping sequence_number time, where time is when the client sends the message
         
-        # FILL IN END
-    except:
-        # Server does not respond
-	# Assume the packet is lost
-        print("Request timed out.")
-        continue
+        try:
+            # Record the "sent time"
+            time_sent = time.time()
 
-# Close the client socket
-clientsocket.close()
- 
+            # Send the UDP packet with the ping message
+            clientSocket.sendto(data.encode(), (host, port))
+
+            # Receive the server response
+            serverResponse, _ = clientSocket.recvfrom(2048)
+      
+            # Record the "received time"
+            time_received = time.time()
+
+            # Display the server response as an output
+            print("server responded: ", serverResponse)
+        
+            # Round trip time is the difference between sent and received time
+            RTT = time_received - time_sent
+
+            
+        except:
+            # Server does not respond
+            # Assume the packet is lost
+            print("Request timed out.")
+            continue
+
+    # Close the client socket
+    clientsocket.close()
+
+if __name__ == "__main__":
+    main()
