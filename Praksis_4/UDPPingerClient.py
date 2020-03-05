9169d8e82ff9987d2271e2cae5e56fea35ebeef7
Marius Bæver Larsen <mariubla@stud.ntnu.no>
2020-03-05 14:40:40 +0100

    Removed unwanted serversocket from client. Changed time to seconds from ms

--- Praksis_4/UDPPingerClient.py
+++ Praksis_4/UDPPingerClient.py
@@ -12,7 +12,6 @@ def main():
      
     # Create UDP client socket
     clientSocket = socket(AF_INET, SOCK_DGRAM) #Internet and UDP
-    clientSocket.bind((host, port))
 
     # Set socket timeout as 1 second
     clientSocket.settimeout(timeout)
@@ -20,18 +19,18 @@ def main():
 
     # Sequence number of the ping message
     ptime = 0  
+    RTT = 0.0
 
     # Ping for 10 times
     while ptime < 10: 
         ptime += 1
-        RTT = 0.0
-        # Format the message to be sent as in the Lab description	
-        data = "Ping\t" + str(ptime) + "\t" + str(RTT) # message should be: Ping sequence_number time, where time is when the client sends the message
-        
         try:
             # Record the "sent time"
-            time_sent = time.time()*1000
-            print("time_sent: ", time_sent)
+            time_sent = time.time()
+            print("time_sent:\t", time_sent)
+
+            # Format the message to be sent as in the Lab description	
+            data = "Ping\t" + str(ptime) + "\t" + str(time_sent) # message should be: Ping sequence_number time, where time is when the client sends the message
 
             # Send the UDP packet with the ping message
             clientSocket.sendto(data.encode(), (host, port))
@@ -40,17 +39,17 @@ def main():
             serverResponse, _ = clientSocket.recvfrom(2048)
       
             # Record the "received time"
-            time_received = time.time()*1000
-            print("time_received: ", time_received)
+            time_received = time.time()
+            print("time_received:\t", time_received)
 
             # Display the server response as an output
-            print("server responded: ", serverResponse.decode())
+            print("server responded:\t", serverResponse.decode())
         
             # Round trip time is the difference between sent and received time
             RTT = time_received - time_sent
-            print("RTT in ms: ", RTT)
+            print("RTT in s:\t", RTT)
+            print("-"*50)
 
-            
         except:
             # Server does not respond
             # Assume the packet is lost
